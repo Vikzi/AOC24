fc7c8c4405b154857cf3866bcca65aa518a06871
Nervetti Viktor <vinerz09@gmail.com>
2024-12-07 14:07:11 +0100

    day seven second part

--- src/DaySeven.php
+++ src/DaySeven.php
@@ -37,15 +37,11 @@ class DaySeven
 
     public function numbersCanBeCombined(int $value, array $numbers): bool
     {
-        echo "CALCULATING " . $value . "\n";
         $maxOperatorNumber = bindec(str_pad("", count($numbers) - 1, '1'));
-        echo "MAX OPERATOR NUMBER " . $maxOperatorNumber . "\n";
         $i = 0;
         while($i <= $maxOperatorNumber) {
             $operatorNumber = decbin($i);
             $operatorDescriptor = str_pad($operatorNumber, count($numbers) - 1, '0', STR_PAD_LEFT);
-            echo "LINE " . $value . "\n";
-            echo "DESCRIPTOR " . $i . "\n";
 
             $val = $operatorDescriptor[0] == '0' ? $numbers[0] + $numbers[1] : $numbers[0] * $numbers[1];
             for ($j = 1; $j < count($numbers) - 1; $j++) {
@@ -55,8 +51,6 @@ class DaySeven
                     $val *= $numbers[$j+1];
                 }
             }
-
-            echo "VALUE " . $val . "\n";
 
             if ($val == $value) {
                 return true;
@@ -70,6 +64,47 @@ class DaySeven
 
     public function solveB(): void
     {
+        $data = $this->getData();
 
+        $sum = 0;
+        foreach ($data as $line) {
+            if ($this->numbersCanBeCombinedWithConcat($line['value'], $line['numbers'])) {
+                $sum += $line['value'];
+            }
+        }
+
+        echo $sum;
+    }
+
+    private function numbersCanBeCombinedWithConcat(int $value, array $numbers): bool
+    {
+        return 
+            $this->recursiveCombination($value, $numbers, 'add', $numbers[0]) ||
+            $this->recursiveCombination($value, $numbers, 'mul', $numbers[0]) ||
+            $this->recursiveCombination($value, $numbers, 'con', $numbers[0]);
+    }
+
+    private function recursiveCombination(int $value, array $numbers, string $operator, int $currentValue, int $index = 1): bool
+    {
+        if ($operator == 'add') {
+            $currentValue = $currentValue + $numbers[$index];
+        } elseif($operator == 'mul') {
+            $currentValue = $currentValue * $numbers[$index];
+        } elseif ($operator == 'con') {
+            $currentValue = intval((string)$currentValue . (string)$numbers[$index]);
+        }
+
+        if ($currentValue > $value) {
+            return false;
+        }
+
+        if ($index == count($numbers) - 1) {
+            return $currentValue == $value;
+        }
+
+        return
+            $this->recursiveCombination($value, $numbers, 'add', $currentValue, $index + 1) ||
+            $this->recursiveCombination($value, $numbers, 'mul', $currentValue, $index + 1) ||
+            $this->recursiveCombination($value, $numbers, 'con', $currentValue, $index + 1);
     }
 }
--- src/main.php
+++ src/main.php
@@ -3,6 +3,12 @@
 require_once 'DaySeven.php';
 
 $solver = new DaySeven();
+$aStart = floor(microtime(true) * 1000);
 $solver->solveA();
-echo "\n";
+$aEnd = $milliseconds = floor(microtime(true) * 1000);
+echo " runtime: " . ($aEnd - $aStart) . "\n";
+
+$bStart = floor(microtime(true) * 1000);
 $solver->solveB();
+$bEnd = $milliseconds = floor(microtime(true) * 1000);
+echo " runtime: " . ($bEnd - $bStart) . "\n";
